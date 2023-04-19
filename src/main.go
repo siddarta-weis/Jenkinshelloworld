d13dc5f882a59dd7db1e58b407513b9e03199b0c
Siddarta Weissbock <siddarta.weis@gmail.com>
2023-04-19 15:01:21 -0300

    feat: hello world

---
+++ src/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+    http.HandleFunc("/", HelloServer)
+    fmt.Println("Listening on port 9080")
+    http.ListenAndServe(":9080", nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello world")
+}
